a1dc70432bb916336e95184e8a9b2326996588f5
efficient-life <efficient122@gmail.com>
2022-03-12 19:21:48 +0900

    first

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,43 @@
 module.exports = {
-  siteMetadata: {
-      title: ``,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: [{
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "https://github.com/"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+	siteMetadata: {
+		title: `My First Gatsby Site`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: [
+		{
+			resolve: 'gatsby-plugin-google-analytics',
+			options: {
+				trackingId: 'https://github.com/',
+			},
+		},
+		'gatsby-plugin-image',
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-sitemap',
+		'gatsby-plugin-mdx',
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: './src/images/',
+			},
+			__key: 'images',
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'pages',
+				path: './src/pages/',
+			},
+			__key: 'pages',
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: `blog`,
+				path: `${__dirname}/blog`,
+			},
+		},
+	],
 };
